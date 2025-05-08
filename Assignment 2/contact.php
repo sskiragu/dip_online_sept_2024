f33bc008f5e9e5546ac450cb4ea3ff59c463d017
sskiragu <skiragu18@gmail.com>
2025-05-08 18:39:38 +0300

    Assignment 2

---
+++ Assignment 2/contact.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact</title>
+    <style>
+        nav li{
+            display: inline;
+            margin-right: 10px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+
+        <?php
+        // Include the navigation bar
+        include_once 'navbar.php';
+        ?>
+
+        <!-- Main Section -->
+        <h1>Contact us Page</h1>
+
+        <!-- Footer Section -->
+        <footer>
+            <p>&copy; 2023 My Website</p>
+        </footer>
+    </div>
+</body>
+</html>
---
+++ Assignment 2/navbar.php
@@ -0,0 +1,8 @@
+<!-- Navigation -->
+<nav>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="about.php">About</a></li>
+        <li><a href="contact.php">Contact</a></li>
+    </ul>
+</nav>
